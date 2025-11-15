21be78aeedc933b81f8f835a38658de15b9f508d
jettaprtrishschupp <jettaprtrishschupp@gmail.com>
2025-11-15 13:36:58 +0100

    New code: “normalize_title” now lives in vaultweaver/exporters/__init__.py

--- vaultweaver/exporters/__init__.py
+++ vaultweaver/exporters/__init__.py
@@ -1,2 +1,8 @@
 
 # autosave 2025-10-03T11:41:09.984145
+
+# --- snippet: normalize_title ---
+def normalize_title(title: str) -> str:
+    return " ".join(title.strip().split())
+# --- endsnippet ---
+
