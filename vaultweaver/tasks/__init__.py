799d79923de78ec11bac42320fd4750f3d323f82
jettaprtrishschupp <jettaprtrishschupp@gmail.com>
2026-06-08 15:35:02 +0200

    Drop “backlinks_map” rfom vaultweaver/tasks/__init__.py

--- vaultweaver/tasks/__init__.py
+++ vaultweaver/tasks/__init__.py
@@ -2,17 +2,7 @@
 
 
 
-# --- snippet: backlinks_map ---
 
-# --- snippet: guess_language ---
-def guess_language(text: str) -> str:
-    import re
-    cyr = len(re.findall(r"[А-Яа-яЁё]", text))
-    lat = len(re.findall(r"[A-Za-z]", text))
-    if cyr > lat: return "ru"
-    if lat > cyr: return "en"
-    return "unknown"
-# --- endsnippet ---
 
 def backlinks_map(index: dict) -> dict:
     back = {k: [] for k in index.keys()}
